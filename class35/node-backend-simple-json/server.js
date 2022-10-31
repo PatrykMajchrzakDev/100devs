162d37c6610e1f09172d94e1f3a2dbb3826de0d3
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2022-10-31 12:15:53 +0100

    Made server.js code look way cleaner and more DRY. Added server-based coin flip api

--- class35/node-backend-simple-json/server.js
+++ class35/node-backend-simple-json/server.js
@@ -1,78 +1,89 @@
-const http = require('http');
-const fs = require('fs')
-const url = require('url');
-const querystring = require('querystring');
-const figlet = require('figlet')
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const querystring = require("querystring");
+const figlet = require("figlet");
 
 const server = http.createServer((req, res) => {
+  const readWriteFile = (file, contentType) => {
+    fs.readFile(file, function (err, data) {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.write(data);
+      res.end();
+    });
+  };
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
-    figlet('404!!', function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
+
+  switch (page) {
+    case "/":
+      readWriteFile("index.html", "text/html");
+      break;
+    case "/otherpage":
+      readWriteFile("otherpage.html", "text/html");
+      break;
+    case "/otherotherpage":
+      readWriteFile("otherotherpage.html", "text/html");
+      break;
+    case "/api":
+      let flipResult = "";
+      if (params["student"] == "flip") {
+        flipResult = Math.random() <= 0.5 ? "heads" : "tails";
+      }
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const objToJson = {
+        name: flipResult,
+      };
+      res.end(JSON.stringify(objToJson));
+      break;
+    case "/css/style.css":
+      fs.readFile("css/style.css", function (err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case "/js/main.js":
+      readWriteFile("js/main.js", "text/javascript");
+      break;
+    default:
+      //Error catch
+      figlet("404!!", function (err, data) {
+        if (err) {
+          console.log("Something went wrong...");
           console.dir(err);
           return;
-      }
-      res.write(data);
-      res.end();
-    });
+        }
+        res.write(data);
+        res.end();
+      });
+      break;
   }
+
+  //Page redirection
+  //else if
 });
 
 server.listen(8000);
+
+//This was previous api before coin flip
+
+//    case "/api":
+// let personName = "unknown";
+// let personStatus = "unknown";
+// let personOccupation = "unknown";
+// if ("student" in params) {
+//   if (params["student"] == "leon") {
+//     personName = "Leon";
+//     personStatus = "Boss man";
+//     personOccupation = "Baller";
+//   }
+// }
+// res.writeHead(200, { "Content-Type": "application/json" });
+// const objToJson = {
+//   name: personName,
+//   status: personStatus,
+//   currentOccupation: personOccupation,
+// };
+// res.end(JSON.stringify(objToJson));
+// break;
